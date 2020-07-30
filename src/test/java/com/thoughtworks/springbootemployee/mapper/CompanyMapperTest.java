5d00b973b85a022db6b43afca9fe5705850a484f
xhz <747367352@qq.com>
2020-07-30 23:16:47 +0800

    [alex xu] feature: add should_return_employee_respond_when_mapper_given_employee test

--- src/test/java/com/thoughtworks/springbootemployee/mapper/CompanyMapperTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/mapper/CompanyMapperTest.java
@@ -1,6 +1,7 @@
 package com.thoughtworks.springbootemployee.mapper;
 
 import com.thoughtworks.springbootemployee.dto.CompanyRequest;
+import com.thoughtworks.springbootemployee.dto.CompanyRespond;
 import com.thoughtworks.springbootemployee.model.Company;
 import org.junit.jupiter.api.Test;
 
@@ -20,5 +21,18 @@ public class CompanyMapperTest {
         assertEquals(company.getCompanyName(), companyRequest.getCompanyName());
         assertEquals(company.getEmployeesNumber(), companyRequest.getEmployeesNumber());
         assertEquals(company.getEmployees(), companyRequest.getEmployees());
+    }
+
+    @Test
+    void should_return_employee_respond_when_mapper_given_employee() {
+        //given
+        Company company = new Company(1, "ali", 1000, new ArrayList<>());
+        //when
+        CompanyRespond companyRespond = CompanyMapper.map(company);
+        //then
+        assertEquals(company.getId(), companyRespond.getId());
+        assertEquals(company.getCompanyName(), companyRespond.getCompanyName());
+        assertEquals(company.getEmployeesNumber(), companyRespond.getEmployeesNumber());
+        assertEquals(company.getEmployees(), companyRespond.getEmployees());
     }
 }
